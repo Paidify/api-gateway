499a5869ed98f7e1e82f00c7a3ac44a2e4dac9d9
vergaraldvm <leonardo11vergara11@gmail.com>
2022-11-16 15:43:10 -0500

    recongnizing /pv1/ay-reqs and /Ãv1/pay-settled

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,1 +1,7 @@
 export const MAX_FAIL_COUNT = 2;
+export const SERVICES =  {
+    'queries': ['users', 'guests', 'pay-concepts', 'pay-methods', 'payments', 'invoices', 'pay-reqs', 'pay-settled'],
+    'auth': ['login'],
+    'payment': ['pay'],
+    'balance': ['checkBalance']
+};
--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,12 +1,8 @@
+import { SERVICES } from "../config/constants.js";
+
 export default function getResourceService (resource) {
-    const services =  {
-        'queries': ['users', 'guests', 'pay-concepts', 'pay-methods', 'payments', 'invoices'],
-        'auth': ['login'],
-        'payment': ['pay'],
-        'balance': ['checkBalance']
-    };
-    for(const service in services) {
-        if(services[service].includes(resource)) return service;
+    for(const service in SERVICES) {
+        if(SERVICES[service].includes(resource)) return service;
     }
     return null;
 }
