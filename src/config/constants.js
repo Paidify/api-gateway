fbc928a6bbcf2e91ee6d098144bb893f834e6de7
vergaraldvm <leonardo11vergara11@gmail.com>
2022-11-20 03:46:30 -0500

    C:/Program Files/Git/campuses

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,6 +1,6 @@
 export const MAX_FAIL_COUNT = 3;
 export const SERVICES =  {
-    'queries': ['users', 'guests', 'pay-concepts', 'pay-concept-persons', 'pay-methods', 'payments', 'invoices', 'pay-reqs', 'pay-settled'],
+    'queries': ['users', 'guests', 'pay-concepts', 'pay-concept-persons', 'pay-methods', 'payments', 'invoices', 'pay-reqs', 'pay-settled', 'campuses'],
     'auth': ['login'],
     'payment': ['pay', 'serve-queue'],
     'balance': ['check-balance']
